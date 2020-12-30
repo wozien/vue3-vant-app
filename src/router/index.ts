018b3e3528626d16ed3ef059603eca1784dbb2d5
wozien <571455773@qq.com>
2020-12-30 09:46:26 +0800

    add:router base

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,8 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 import { LocalStorageKeys } from '@/assets/js/constant'
 
+const base = process.env.NODE_ENV === 'production' ? '/mobile' : '/'
+
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(base),
   routes
 })
 
