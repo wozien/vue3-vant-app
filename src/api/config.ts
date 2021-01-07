2b6a815bb851360cdaa4834eaf299140ba67c0b7
wozien <571455773@qq.com>
2021-01-07 10:38:14 +0800

    config

--- src/api/config.ts
+++ src/api/config.ts
@@ -7,10 +7,10 @@ export default {
 
   IS_PROD: isProd, 
 
-  BASE_URL: isProd ? 'http://odoo.studio.insuite.cn': 'http://182.92.100.160:18080',
+  BASE_URL: wxEnv === 'dev' ? 'http://182.92.100.160:18080' : 'http://odoo.studio.insuite.cn',
 
   WX_APP_ID: wxEnv === 'dev' ? 'wxef1dd7f831c0b5f6' :
-    (wxEnv === 'uat' ? 'wxef1dd7f831c0b5f6' : 'wx33e995dcf375d68a'),
+    (wxEnv === 'uat' ? 'wx59392d14e4411f29' : 'wx33e995dcf375d68a'),
     
   WX_OPEN_ID: 'oWkYa5j_tQDiPWuWvdcbimgI40Tk'
 }
