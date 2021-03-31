6947a40118d8874b9f1e657c4c41d5e47f554dbb
wozien <571455773@qq.com>
2021-03-31 15:31:48 +0800

    chore: commit-msg verify

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -5,20 +5,6 @@ const msg = require('fs')
   .readFileSync(msgPath, 'utf-8')
   .trim()
 
-/**
- * revert: 回滚某次commit
- * feat 新功能
- * fix bug修复
- * docs 文档修改
- * style 格式变动，不影响运行结果
- * refactor  重构
- * test  单元测试
- * build  构建或者辅助工具变动
- * chore  杂项，不包含源代码
- * ci 自动流程工具
- * perf  性能相关
- * types  ts 类型定义
- */
 const commitRE = /^(revert: )?(feat|fix|docs|style|refactor|perf|test|build|chore|types|ci)(\(.+\))?: .{1,50}/
 
 if (!commitRE.test(msg)) {
