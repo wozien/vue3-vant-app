93d08ae6dec092d562e17938fb27081dc21b80ae
wozien <571455773@qq.com>
2021-04-06 01:18:45 +0800

    feat: page switch transition

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="layout-page">
-    <router-view v-title="$route.meta.title" :style="{height: contentHeight + 'px'}" v-slot="{ Component }"> 
-      <keep-alive :exclude="['View', 'Flow']">
-        <component :is="Component" />
-      </keep-alive>
+    <router-view
+      v-title="$route.meta.title"
+      :style="{ height: contentHeight + 'px' }"
+      v-slot="{ Component }"
+    >
+      <transition :name="transitionName">
+        <keep-alive :exclude="['View', 'Flow']">
+          <component :is="Component" />
+        </keep-alive>
+      </transition>
     </router-view>
     <van-tabbar v-model="active" v-if="hasTab">
       <van-tabbar-item name="dashboard" icon="wap-home-o" to="/dashboard">首页</van-tabbar-item>
@@ -14,10 +20,12 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed, watchEffect } from 'vue'
+import { defineComponent, ref, computed, watchEffect, watch } from 'vue'
 import { useStore } from '@/store'
 import { useRoute } from 'vue-router'
 import { isWechatAgent } from '@/helpers/utils'
+
+let refreshed = false
 
 export default defineComponent({
   setup() {
@@ -26,6 +34,7 @@ export default defineComponent({
     const store = useStore()
 
     const active = ref('')
+    const transitionName = ref('')
     const contentHeight = ref(document.body.clientHeight)
     const hasTab = computed(() => !!route.meta.tab)
 
@@ -37,16 +46,16 @@ export default defineComponent({
     // 加载用户信息
     store.dispatch('setUserInfo')
     store.dispatch('setOrgs')
-    
+
     watchEffect(() => {
       active.value = route.path.substr(1)
     })
 
     watchEffect(() => {
-      if(hasTab.value)  {
-        // nothing todo, just help to trigger effect 
+      if (hasTab.value) {
+        // nothing todo, just help to trigger effect
       }
-      if(isWechatAgent({ iphone: true })) {
+      if (isWechatAgent({ iphone: true })) {
         // ios 微信客户端需要延时计算高度
         setTimeout(() => {
           calcHeight()
@@ -54,17 +63,68 @@ export default defineComponent({
       } else calcHeight()
     })
 
+    watch(
+      [
+        () => route.fullPath,
+        () => route.meta.pageIndex as number,
+        () => route.query.viewType as string,
+      ],
+      ([path, index = 0, viewType], [oldPath, oldIndex = 0, oldViewType]) => {
+        if (!refreshed) {
+          refreshed = true
+          return
+        }
+        console.log(oldPath, path)
+        if (path !== oldPath && index !== oldIndex) {
+          if (index > oldIndex) {
+            transitionName.value = 'forward'
+          } else {
+            transitionName.value = 'back'
+          }
+        } else if (viewType === 'form' && oldViewType === 'list') {
+          transitionName.value = 'forward'
+        } else if (viewType === 'list' && oldViewType === 'form') {
+          transitionName.value = 'back'
+        } else {
+          transitionName.value = ''
+        }
+      }
+    )
+
     return {
       active,
+      transitionName,
       hasTab,
-      contentHeight
+      contentHeight,
     }
-  }
+  },
 })
 </script>
 
 <style lang="less" scoped>
 .layout-page {
   .app-page;
+  overflow: hidden;
+}
+
+.forward-enter-active,
+.forward-leave-active,
+.back-enter-active,
+.back-leave-active {
+  transition: all 0.4s ease;
+  position: absolute;
+  width: 100%;
+}
+
+.forward-enter-from,
+.back-leave-to {
+  opacity: 0.5;
+  transform: translate(100%);
+}
+
+.forward-leave-to,
+.back-enter-from {
+  opacity: 0.5;
+  transform: translateX(-100%);
 }
 </style>
