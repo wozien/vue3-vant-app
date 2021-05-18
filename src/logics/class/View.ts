d85bcd28890b6e65a9df64ee3294993ab62244e2
wozien <571455773@qq.com>
2021-05-18 16:29:11 +0800

    fix: prod环境禁用编辑功能

--- src/logics/class/View.ts
+++ src/logics/class/View.ts
@@ -116,6 +116,16 @@ class View {
       }
     }
 
+    // 生产环境暂时屏蔽编辑态按钮
+    if (import.meta.env.PROD) {
+      viewButtons = viewButtons.filter(btn => {
+        return (
+          btn.mode !== 'edit' &&
+          (!btn.funcName || !['edit', 'create', 'copy'].includes(btn.funcName))
+        )
+      })
+    }
+
     return viewButtons
   }
 
